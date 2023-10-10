dae565b09bb1aaf679e7808bec543ff8431bd2dd
MacBook <mac.window.10@gmail.com>
2023-10-10 18:44:53 +0500

    initial

---
+++ api.php
@@ -0,0 +1,114 @@
+<?php
+ 
+header("Content-Type:application/json");
+include('connection.php');
+
+if(isset($_GET['action']) && $_GET['action']!='')
+{
+	if($_GET['action'] == "login")
+	{
+		if ((isset($_GET['username']) && $_GET['username']!="") && (isset($_GET['password']) && $_GET['password']!="")) {
+			$username = $_GET['username'];
+			$password = $_GET['password'];
+			$query = "SELECT * FROM `users` WHERE username='$username' AND password='$password'";
+			$result = mysqli_query($con, $query);
+			if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+			{
+				$userData['id'] = $row['id'];
+				$userData['username'] = $row['username'];
+				$userData['password'] = $row['password'];
+				$userData['name'] = $row['name'];
+				$userData['cnic'] = $row['cnic'];
+
+				$response["status"] = "true";
+				$response["message"] = "User Details";
+				$response["users"] = $userData;
+			} else {
+				$response["status"] = "true";
+				$response["message"] = "Login failed";
+			}
+		}
+		else{
+			$response["status"] = "false";
+			$response["message"] = "No user(s) found!";
+		}
+	}
+	else if($_GET['action'] == "signup")
+	{
+		if ((isset($_GET['username']) && $_GET['username']!="") && (isset($_GET['password']) && $_GET['password']!="") && (isset($_GET['name']) && $_GET['name']!="") && (isset($_GET['cnic']) && $_GET['cnic']!="") && (isset($_GET['address']) && $_GET['address']!="")) 
+		{
+			$username = $_GET['username'];
+			$password = $_GET['password'];
+			$name = $_GET['name'];
+			$cnic = $_GET['cnic'];
+			$address = $_GET['address'];
+			$query = "SELECT * FROM `users` WHERE username='$username'";
+			$result = mysqli_query($con, $query);
+			if ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+			{
+				$response["status"] = "true";
+				$response["message"] = "Username already exists";
+			} else {
+				$query = "INSERT INTO `users` (username, password, name, cnic, address) VALUES('$username', '$password', '$name', '$cnic', '$address')";
+				if(mysqli_query($con, $query)) {
+					$response["status"] = "true";
+					$response["message"] = "User created";
+				} else{
+					$response["status"] = "true";
+					$response["message"] = "Error in creating user";
+				}
+			}
+		}
+		else{
+			$response["status"] = "false";
+			$response["message"] = "Error in creating user";
+		}
+	}
+	else if($_GET['action'] == "get_restaurants")
+	{
+		$data = array();
+		if (isset($_GET['name'])) {
+			$name = $_GET['name'];
+			if ($name == "") {
+				$query = "SELECT * FROM `restaurant`";
+			} else {
+				$query = "SELECT * FROM `restaurant`";
+			}
+			$result = mysqli_query($con, $query);
+			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+			{
+				$userData['id'] = $row['id'];
+				$userData['name'] = $row['name'];
+				$userData['contact_number'] = $row['contact_number'];
+				$userData['address'] = $row['address'];
+				$userData['city'] = $row['city'];
+				$userData['restaurant_type'] = $row['restaurant_type'];
+				$userData['rating'] = $row['rating'];
+
+				$data[]=array("id"=>$row['id'],"name"=>$row['name'],
+					"contact_number"=>$row['contact_number'],
+					"address"=>$row['address'],
+					"city"=>$row['city'],
+					"restaurant_type"=>$row['restaurant_type'],
+					"rating"=>$row['rating']);
+			} 
+			$response["status"] = "true";
+			$response["message"] = "No quote found";
+			$response["restaurants"] = $data;
+		}
+		else{
+			$response["status"] = "false";
+			$response["message"] = "No restaurant found";
+			$response["restaurants"] = $data;
+		}
+	}
+	else 
+	{
+	}
+} else {
+	$response["status"] = "false";
+	$response["message"] = "Action not provided";
+}
+echo json_encode($response); exit;
+ 
+?>
---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+ $dbhost = "127.0.0.1";
+ $dbuser = "root";
+ $dbpass = "pakistan";
+ $db = "food_buddy";
+ $con = mysqli_connect($dbhost, $dbuser, $dbpass , $db) or die($con);
+?>
