69e1319993f592f26d2cc64d88bcf6bbfbf54c68
MacBook <mac.window.10@gmail.com>
2023-10-14 22:04:06 +0500

    db backup

--- api.php
+++ api.php
@@ -84,13 +84,15 @@ if(isset($_GET['action']) && $_GET['action']!='')
 				$userData['city'] = $row['city'];
 				$userData['restaurant_type'] = $row['restaurant_type'];
 				$userData['rating'] = $row['rating'];
+				$userData['image_filename'] = $row['image_filename'];
 
 				$data[]=array("id"=>$row['id'],"name"=>$row['name'],
 					"contact_number"=>$row['contact_number'],
 					"address"=>$row['address'],
 					"city"=>$row['city'],
 					"restaurant_type"=>$row['restaurant_type'],
-					"rating"=>$row['rating']);
+					"rating"=>$row['rating'],
+					"image_filename"=>$row['image_filename']);
 			} 
 			$response["status"] = "true";
 			$response["message"] = "No quote found";
@@ -100,6 +102,39 @@ if(isset($_GET['action']) && $_GET['action']!='')
 			$response["status"] = "false";
 			$response["message"] = "No restaurant found";
 			$response["restaurants"] = $data;
+		}
+	}
+	else if($_GET['action'] == "get_menus")
+	{
+		$data = array();
+		if (isset($_GET['restaurant_id'])) {
+			$restaurant_id = $_GET['restaurant_id'];
+			$query = "SELECT * FROM `menu` WHERE restaurant_id=$restaurant_id";
+			$result = mysqli_query($con, $query);
+			while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+			{
+				$userData['id'] = $row['id'];
+				$userData['name'] = $row['name'];
+				$userData['description'] = $row['description'];
+				$userData['preparation_time'] = $row['preparation_time'];
+				$userData['price'] = $row['price'];
+				$userData['image_filename'] = $row['image_filename'];
+
+				$data[]=array("id"=>$row['id'],
+					"name"=>$row['name'],
+					"description"=>$row['description'],
+					"preparation_time"=>$row['preparation_time'],
+					"price"=>$row['price'],
+					"image_filename"=>$row['image_filename']);
+			} 
+			$response["status"] = "true";
+			$response["message"] = "No quote found";
+			$response["menus"] = $data;
+		}
+		else{
+			$response["status"] = "false";
+			$response["message"] = "No restaurant found";
+			$response["menus"] = $data;
 		}
 	}
 	else 
